58970451a68c61156f35e85379e9d11d4b3cb873
iqqtaa <argo.suranata@mhs.unsoed.ac.id>
2026-06-01 00:58:16 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'username' => 'admin',
-            'password' => 'admin',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin'),
         ]);
 
         $this->call(ExpertSystemSeeder::class);
