afefd3a9749380aa9f4faad661582b1a6952ae8f
iqqtaa <argo.suranata@mhs.unsoed.ac.id>
2026-06-01 00:41:20 +0700

    fake()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'username' => fake()->userName(),
+            'username' => $this->faker->userName(),
             'password' => static::$password ??= Hash::make('password'),
         ];
     }
